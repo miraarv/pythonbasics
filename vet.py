094a0b8cf461092e1a1ab4ce21059d0c36cbcd1c
mira <miraferreiravv@gmail.com>
2023-08-24 19:33:26 -0300

    sss

--- vet.py
+++ vet.py
@@ -1,8 +1,8 @@
 #variável composta homogênea 24/08/2023
 
 qtd = 5
-vetor = []
+n = []
 
 for i in range(qtd):
-    vetor.append(float(input("Informe o número: ")))
-print(vetor)
+    n.append(float(input("Informe o número: ")))
+print(n)
