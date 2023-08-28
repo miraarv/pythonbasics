76062e830cf25fe24f7a79ee57f1c9822d0d5c9f
mira <miraferreiravv@gmail.com>
2023-08-28 18:47:37 -0300

    ok

--- vet.py
+++ vet.py
@@ -2,7 +2,18 @@
 
 qtd = 5
 n = []
-
+soma = 0
+media = 0
+cont = 0
 for i in range(qtd):
     n.append(float(input("Informe o número: ")))
-print(n)
+    print(n)
+    soma = soma + n[i]
+    media = soma/qtd
+    if n[i]>media:
+        cont = cont + 1 
+
+print("\n")
+print("Média:", media)
+print("Vetor resultante:\n",n)
+print("Quantidade de vetores maiores que a média: ", cont)
