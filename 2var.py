a97dcdbf07a401241b778d1bcd5c5bd9fdcec599
mira <miraferreiravv@gmail.com>
2023-08-29 19:25:21 -0300

    ok

--- 2var.py
+++ 2var.py
@@ -1,6 +1,5 @@
 soma = 0
-
 for i in range (2):
-    x = float(input("Insira a nota do",i,"º bimestre:\n"))
+    x = float(input("Insira as notas do aluno:\n"))
     soma= soma + x
 print("A média das notas inseridas é: ", (soma/2))
