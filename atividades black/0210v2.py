dab911970c876a6fea6d838cd5028587152217bf
mira <miraferreiravv@gmail.com>
2023-09-12 19:25:11 -0300

    ok

--- atividades black/0210v2.py
+++ atividades black/0210v2.py
@@ -2,19 +2,23 @@
 #solicite ao usuário que digite vários números e imprima quantos destes números
 #são pares e quantos são ímpares. O programa deve parar de solicitar números
 #quando o usuário digitar o número zero.
-par=0
-impar=0
+par=[]
+impar=[]
 n=int(input("Insira um número: "))
 if n%2==0:
-        par=par+1
-else:
-        impar=impar+1
+    par.append(n)
+if n%2==1:
+    impar.append(n)
+
 while n!=0:
     n=int(input("Insira um número: "))
     if n%2==0:
-        par=par+1
-    else:
-        impar=impar+1
+        par.append(n)
+    if n%2==1:
+        impar.append(n)
 
-print("\nQuantidade de números pares: ", par)
-print("\nQuantidade de números ímpares: ", impar)
+np=len(par)
+ni=len(impar)
+
+print("\nQuantidade de números pares: ", np)
+print("\nQuantidade de números ímpares: ", ni)
